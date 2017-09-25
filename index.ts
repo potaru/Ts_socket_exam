c3dfe7252e8cd630ace2b04a0063a7af6adb59c5
potaru <jspaulbhang@gmail.com>
2017-09-25 11:56:13 +0900

    모듈화

--- index.ts
+++ index.ts
@@ -5,16 +5,11 @@ import * as iox from 'socket.io';
 import config from "./config/config";
 import { router as main } from './router/main';
 
-
 declare var __dirname;
 
 const app = appx();
 const http = httpx.createServer(app);
 const io = iox(http);
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/views/index.html');
-});
 
 app.use(appx.static(__dirname+'/public'));
 app.use('/', main);
